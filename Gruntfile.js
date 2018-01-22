2d02d5c307324f58b21afbe34c52170030a5ae41
Aashni Shah <contact@aashni.me>
2018-01-22 03:41:17 -0800

    adding forgotten files for deployment step

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,9 @@
 
 module.exports = function (grunt) {
 
+  grunt.loadNpmTasks('grunt-build-control');
+  var pkg = require('./package.json');
+
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
@@ -415,6 +418,27 @@ module.exports = function (grunt) {
         'imagemin',
         'svgmin'
       ]
+    },
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:<username>/<githubrepo>.git',
+          branch: 'gh-pages'
+        }
+      },
+      local: {
+        options: {
+          remote: '../',
+          branch: 'build'
+        }
+      }
     },
 
     // Test settings
